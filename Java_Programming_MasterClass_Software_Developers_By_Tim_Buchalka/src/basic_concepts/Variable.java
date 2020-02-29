2d5e1a95749dc901d8733c80b3b52266cb5d58c4
TARANVIR SINGH SAINI <taranvir.554@gmail.com>
2020-03-01 02:21:54 +0530

    Commit

---
+++ Java_Programming_MasterClass_Software_Developers_By_Tim_Buchalka/src/basic_concepts/Variable.java
@@ -0,0 +1,14 @@
+package basic_concepts;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Hello, Tim");
+
+	        int myFirstNumber = (10 + 5) + (2 * 10);
+	        System.out.println(myFirstNumber);
+
+	}
+
+}
